0160cfdec724c464ccfa458d8c55f4319cf9b5b6
guojian <guojian@hibee.com>
2022-03-10 13:15:02 +0800

    add 21 solution

--- 21.合并两个有序链表.swift
+++ 21.合并两个有序链表.swift
@@ -23,6 +23,15 @@
  * 
  */
 
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
 // @lc code=start
 /**
  * Definition for singly-linked list.
@@ -37,7 +46,57 @@
  */
 class Solution {
     func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        
+        guard let l1 = l1, let l2 = l2 else {
+            return l1 ?? l2
+        }
+        let head = ListNode(0)
+        var p: ListNode? = head
+        var op1: ListNode? = l1
+        var op2: ListNode? = l2
+        while let p1 = op1, let p2 = op2 {
+            if p1.val <= p2.val {
+                p?.next = p1
+                op1 = op1?.next
+            } else {
+                p?.next = p2
+                op2 = op2?.next
+            }
+            p = p?.next
+        }
+        if let p1 = op1 {
+            p?.next = p1
+        }
+        if let p2 = op2 {
+            p?.next = p2
+        }
+        return head.next
     }
 }
 // @lc code=end
+
+extension ListNode {
+    var log: String {
+        var msg = "\(val)"
+        var p: ListNode? = self
+        while p?.next != nil {
+            if let val = p?.next?.val {
+                msg += "->\(val)"
+            }
+            p = p?.next
+        }
+        return msg
+    }
+}
+
+let l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next?.next = ListNode(4)
+
+let l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next?.next = ListNode(4)
+
+let solution = Solution()
+print("l1: \(l1.log)")
+print("l2: \(l2.log)")
+print("merge: \(solution.mergeTwoLists(l1, l2)?.log ?? "")")
