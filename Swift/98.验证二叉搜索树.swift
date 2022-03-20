fee194d50f009df8b72ea34106562cc191a94cc9
naijoug <naijoug@126.com>
2022-03-20 15:21:06 +0800

    add 98 solution

---
+++ Swift/98.验证二叉搜索树.swift
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=swift
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (35.65%)
+ * Likes:    1473
+ * Dislikes: 0
+ * Total Accepted:    455.1K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * 
+ * 有效 二叉搜索树定义如下：
+ * 
+ * 
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [2,1,3]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [5,1,4,null,null,3,6]
+ * 输出：false
+ * 解释：根节点的值是 5 ，但是右子节点的值是 4 。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围在[1, 10^4] 内
+ * -2^31 <= Node.val <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        isValidBST(root, min: nil, max: nil)
+    }
+    private func isValidBST(_ root: TreeNode?, min: TreeNode?, max: TreeNode?) -> Bool {
+        guard let root = root else { return true }
+        if let min = min, root.val <= min.val { return false }
+        if let max = max, root.val >= max.val { return false }
+        
+        return isValidBST(root.left, min: min, max: root) && isValidBST(root.right, min: root, max: max)
+    }
+}
+// @lc code=end
+
