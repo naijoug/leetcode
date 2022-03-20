52d7794f7d0ad4feba7514d95ee30f56ed8a2b10
naijoug <naijoug@126.com>
2022-03-20 14:55:47 +0800

    add 114 solution

---
+++ Swift/114.二叉树展开为链表.swift
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=swift
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (72.82%)
+ * Likes:    1107
+ * Dislikes: 0
+ * Total Accepted:    230.7K
+ * Total Submissions: 316.9K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ * 
+ * 
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,5,3,4,null,6]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [0]
+ * 输出：[0]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中结点数在范围 [0, 2000] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以使用原地算法（O(1) 额外空间）展开这棵树吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func flatten(_ root: TreeNode?) {
+        guard let root = root else { return }
+        
+        let left = root.left
+        let right = root.right
+        root.left = nil
+        root.right = left
+        var p: TreeNode? = root
+        while p?.right != nil {
+            p = p?.right
+        }
+        p?.right = right
+
+        flatten(root.right)
+    }
+}
+// @lc code=end
+
