a59e910cdaeae18c4629a72a07f92ef274bcbd80
naijoug <naijoug@126.com>
2022-04-12 23:22:54 +0800

    add 567 solution

--- Swift/567.字符串的排列.swift
+++ Swift/567.字符串的排列.swift
@@ -48,8 +48,83 @@
 // @lc code=start
 class Solution {
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
-        
+        let char1s = [Character](s1)
+        let char2s = [Character](s2)
+        var needs = [Character: Int]()
+        for char in char1s {
+            needs[char] = (needs[char] ?? 0) + 1
+        }
+        func isOk() -> Bool {
+            !needs.values.contains(where: { $0 > 0 })
+        }
+        var start = 0
+        var end = 0
+        while end < char2s.count {
+            let char = char2s[end]
+            if needs.keys.contains(char) {
+                needs[char] = (needs[char] ?? 0) - 1
+            }
+            end += 1
+            guard isOk() else { continue }
+            while isOk(), start < end {
+                guard end - start == char1s.count else {
+                    let char = char2s[start]
+                    if needs.keys.contains(char) {
+                        needs[char] = (needs[char] ?? 0) + 1
+                    }
+                    start += 1
+                    continue
+                }
+                return true
+            }
+        }
+        return false
     }
 }
 // @lc code=end
 
+class Solution1 {
+    func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        let count1 = s1.count
+        let count2 = s2.count
+        guard count1 <= count2 else { return false }
+        let matchDict = permute(s1)
+        let chars = [Character](s2)
+        for i in 0..<(count2 - count1 + 1) {
+            let substring = String(chars[i..<(i + count1)])
+            print(substring)
+            guard matchDict[substring] ?? false else { continue }
+            return true
+        }
+        return false
+    }
+    private func permute(_ str: String) -> [String: Bool] {
+        var matchDict = [String: Bool]()
+        func permute(_ chars: [Character], track: [Character] = [], used: [Int: Bool] = [:]) {
+            guard track.count < chars.count else {
+                matchDict[String(track)] = true
+                return
+            }
+            var track = track
+            var used = used
+            for i in 0..<chars.count {
+                guard !(used[i] ?? false) else { continue }
+                let char = chars[i]
+                used[i] = true
+                track.append(char)
+                permute(chars, track: track, used: used)
+                track.removeLast()
+                used[i] = false
+            }
+        }
+        permute([Character](str))
+        print(matchDict)
+        return matchDict
+    }
+}
+
+let solution = Solution()
+print(solution.checkInclusion("ab", "eidbaooo"))
+print(solution.checkInclusion("adc", "dcda"))
+print(solution.checkInclusion("horse", "ros"))
+print(solution.checkInclusion("algorithm", "altruistic"))
