56403cf2e40f8a3e1282fea898971930c5bf88db
naijoug <naijoug@outlook.com>
2023-09-25 22:38:38 +0800

    add 872 solution

---
+++ Swift/872.叶子相似的树.swift
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=872 lang=swift
+ *
+ * [872] 叶子相似的树
+ *
+ * https://leetcode.cn/problems/leaf-similar-trees/description/
+ *
+ * algorithms
+ * Easy (65.02%)
+ * Likes:    218
+ * Dislikes: 0
+ * Total Accepted:    79.8K
+ * Total Submissions: 122.8K
+ * Testcase Example:  '[3,5,1,6,2,9,8,null,null,7,4]\n' +
+  '[3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]'
+ *
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ * 
+ * 
+ * 
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ * 
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ * 
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 =
+ * [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 给定的两棵树结点数在 [1, 200] 范围内
+ * 给定的两棵树上的值在 [0, 200] 范围内
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    private func dfs(_ root: TreeNode?) -> String {
+        guard let root = root else { return "" }
+        guard root.left == nil, root.right == nil else {
+            return dfs(root.left) + dfs(root.right)
+        }
+        return "\(root.val)"
+    }
+    
+    func leafSimilar(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        dfs(root1) == dfs(root2)
+    }
+}
+// @lc code=end
