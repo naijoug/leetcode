a29f7b8dd5558a9516580ac41b8bc37925f0754d
naijoug <naijoug@outlook.com>
2023-10-04 19:08:18 +0800

    add 238 solution

--- Swift/238.除自身以外数组的乘积.swift
+++ Swift/238.除自身以外数组的乘积.swift
@@ -3,44 +3,70 @@
  *
  * [238] 除自身以外数组的乘积
  *
- * https://leetcode-cn.com/problems/product-of-array-except-self/description/
+ * https://leetcode.cn/problems/product-of-array-except-self/description/
  *
  * algorithms
- * Medium (71.20%)
- * Likes:    676
+ * Medium (74.83%)
+ * Likes:    1585
  * Dislikes: 0
- * Total Accepted:    90.5K
- * Total Submissions: 127.2K
+ * Total Accepted:    313.9K
+ * Total Submissions: 419.5K
  * Testcase Example:  '[1,2,3,4]'
  *
- * 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i]
- * 之外其余各元素的乘积。
+ * 给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+ * 
+ * 题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+ * 
+ * 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
  * 
  * 
  * 
- * 示例:
+ * 示例 1:
  * 
- * 输入: [1,2,3,4]
+ * 
+ * 输入: nums = [1,2,3,4]
  * 输出: [24,12,8,6]
  * 
  * 
+ * 示例 2:
  * 
- * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
  * 
- * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+ * 输入: nums = [-1,1,0,-3,3]
+ * 输出: [0,0,9,0,0]
  * 
- * 进阶：
- * 你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 2 <= nums.length <= 10^5
+ * -30 <= nums[i] <= 30
+ * 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
  * 
  */
 
 // @lc code=start
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var product = 1
-        for num in nums {
-            product *= num
+        var result = [Int].init(repeating: 0, count: nums.count)
+        result[0] = 1
+        for i in 1..<nums.count {
+            result[i] = result[i - 1] * nums[i - 1]
         }
+        
+        var temp = 1
+        for i in stride(from: nums.count - 1, through: 0, by: -1) {
+            result[i] *= temp
+            temp *= nums[i]
+        }
+        return result
     }
 }
 // @lc code=end
+
