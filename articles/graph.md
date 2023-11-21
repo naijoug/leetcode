e1b23200e2d119f6a43d44a93beec5a442b03f38
naijoug <naijoug@outlook.com>
2023-11-21 09:40:38 +0800

    update README

--- articles/graph.md
+++
@@ -1,8 +0,0 @@
-# Graph - “图”
-
-## `DFS`(Depth First Search) - “深度优先搜索”
-
-
-
-## `BFS`(Breadth First Search) - “广度优先搜索”
-
---
+++ solutions/342.4-的幂.swift
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=342 lang=swift
+ *
+ * [342] 4的幂
+ *
+ * https://leetcode.cn/problems/power-of-four/description/
+ *
+ * algorithms
+ * Easy (52.93%)
+ * Likes:    355
+ * Dislikes: 0
+ * Total Accepted:    139.1K
+ * Total Submissions: 262.5K
+ * Testcase Example:  '16'
+ *
+ * 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+ * 
+ * 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4^x
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 16
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 5
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：n = 1
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * -2^31 <= n <= 2^31 - 1
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能不使用循环或者递归来完成本题吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    func isPowerOfFour(_ n: Int) -> Bool {
+
+    }
+}
+// @lc code=end
+
